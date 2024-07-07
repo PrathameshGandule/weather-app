d2227cbc8bd590c2f37573364d3be9bacbc7902d
PrathameshGandule <prathameshgandule1@gmail.com>
2024-07-07 19:49:51 +0530

    made changes to env variables

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = secrets.APIKEY;
+const apiKey = process.env.APIKEY;
 
 
 weatherForm.addEventListener("submit", async event => {
